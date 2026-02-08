5b5c8b57d9f0e52124acfda13478ec2e95a2ac8e
mihotaka9116 <mihotaka9116@gmail.com>
2026-02-09 00:41:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ const fadeObserver = new IntersectionObserver((entries) => {
 document.querySelectorAll('.fade-in').forEach(target => {
     fadeObserver.observe(target);
 });
+
+
+// スクロールに合わせてふわっと表示させるスクリプト
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('is-show');
+    }
+  });
+}, { threshold: 0.2 });
+
+document.querySelectorAll('.js-fade-up').forEach(el => observer.observe(el));
+
